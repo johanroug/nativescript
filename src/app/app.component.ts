5ab1b2111d284a7d4e8fda62333f2e3aca3dee53
Johan Roug <johanroug@gmail.com>
2019-10-02 10:12:43 +0200

    no message

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { WebView, LoadEventData } from "tns-core-modules/ui/web-view";
+
+@Component({
+    selector: "ns-app",
+    templateUrl: "./app.component.html"
+})
+export class AppComponent {
+
+ }
---
+++ src/app/layouts/stack/stack.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-stack',
+  moduleId: module.id,
+  templateUrl: './stack.component.html',
+  styleUrls: ['./stack.component.css']
+})
+export class StackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
