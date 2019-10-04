f9e2209a6c2afa4ae35638901d1b6be5ba0c658f
Johan Roug <johanroug@gmail.com>
2019-10-04 22:44:33 +0200

    no message

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ns-auth',
@@ -8,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSigninHandler() {
+    this._router.navigate(['/today']);
   }
 
 }
